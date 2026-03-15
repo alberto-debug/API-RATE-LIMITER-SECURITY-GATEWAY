0be83578723397233161be73228fcfa816b99143
Alberto-debug <albertojunior848423803@gmail.com>
2026-03-15 16:00:11 +0300

    rate Limiter services implementation after util

---
+++ src/main/java/com/alberto/rateLimiter/annotation/RateLimit.java
@@ -0,0 +1,14 @@
+package com.alberto.rateLimiter.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RateLimit {
+
+    int requestPerMinute() default 100;
+    int windowSizeSeconds() default 60;
+}
